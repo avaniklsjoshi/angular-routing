e5d87762981236fde6b1a4dbdab3ca3df090b7a6
Avani joshi <avaniklsjoshi@gmail.com>
2017-07-19 10:30:32 +0530

    basic pages created

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {appRouting} from './app.routing';
+
+@NgModule({
+  imports:[
+    BrowserModule,
+    FormsModule,
+    appRouting],
+  declarations:[
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent],
+  bootstrap:[AppComponent]
+})
+
+export class AppModule{}
---
+++ app/contact/contact.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'contact-page',
+  template:`
+  i am contact`
+})
+export class ContactComponent{}
